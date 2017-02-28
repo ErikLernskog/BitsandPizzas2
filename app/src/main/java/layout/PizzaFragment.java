a80a5de1cc7f9c954c39b6619dec42fb6da1c7db
ErikLernskog <funbo@hotmail.com>
2017-02-28 07:59:02 +0100

    Beginning of drawer

---
+++ app/src/main/java/layout/PizzaFragment.java
@@ -0,0 +1,34 @@
+package layout;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.hfad.bitsandpizzas.R;
+
+public class PizzaFragment extends ListFragment {
+
+
+    public PizzaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_2,
+                getResources().getStringArray(R.array.pizzas)
+        );
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
