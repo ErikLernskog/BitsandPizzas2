a58401a976d63f3f51eda25a8e8817728c00c86f
ErikLernskog <funbo@hotmail.com>
2017-03-31 08:20:25 +0200

    Pizza.java done and dependency added

--- app/src/main/java/com/hfad/bitsandpizzas/Pizza.java
+++ app/src/main/java/com/hfad/bitsandpizzas/Pizza.java
@@ -1,8 +1,24 @@
 package com.hfad.bitsandpizzas;
 
-/**
- * Created by ETXERAN on 2017-03-31.
- */
-
 public class Pizza {
+    private String name;
+    private int imageResourceId;
+
+    public static final Pizza[] pizzas = {
+            new Pizza("Diavlo", R.drawable.diavolo),
+            new Pizza("Funghi", R.drawable.funghi)
+    };
+
+    private Pizza(String name, int imageResourceId) {
+        this.name = name;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
 }
